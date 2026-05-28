8ab3f9eeab1abbe0ee156af6941b2b2f39800823
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 15:50:03 +0530

    Update embedding_service.py

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -1,9 +1,13 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer(
+"all-MiniLM-L6-v2",
+device="cpu"
+)
 
 def create_embeddings(chunks):
-
-    embeddings = model.encode(chunks)
-
-    return embeddings
+embeddings = model.encode(
+chunks,
+convert_to_numpy=True
+)
+return embeddings
