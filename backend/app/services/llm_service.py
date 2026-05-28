f088a19323fc34c9b1417a12ed68ed59f74565b6
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 22:04:48 +0530

    Update llm_service.py

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -1,7 +1,11 @@
+import os
+
 from langchain_groq import ChatGroq
-from app.core.config import GROQ_API_KEY
+from dotenv import load_dotenv
+
+load_dotenv()
 
 llm = ChatGroq(
-    groq_api_key=GROQ_API_KEY,
+    groq_api_key=os.getenv("GROQ_API_KEY"),
     model_name="llama3-8b-8192"
 )
