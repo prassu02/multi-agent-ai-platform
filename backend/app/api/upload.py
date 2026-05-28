5702eb282818b8b9eed305d4d457abe2c9e86017
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 23:26:29 +0530

    Update upload.py

--- backend/app/api/upload.py
+++ backend/app/api/upload.py
@@ -8,14 +8,47 @@ UPLOAD_DIR = "uploads"
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+
 @router.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
 
     try:
-        file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+        # =========================
+        # FILE VALIDATION
+        # =========================
+
+        if not file.filename.endswith(".pdf"):
+
+            return {
+                "error": "Only PDF files are allowed"
+            }
+
+        # =========================
+        # SAVE FILE
+        # =========================
+
+        file_path = os.path.join(
+            UPLOAD_DIR,
+            file.filename
+        )
 
         with open(file_path, "wb") as f:
-            f.write(await file.read())
+
+            contents = await file.read()
+
+            # LIMIT FILE SIZE (5MB)
+            if len(contents) > 5 * 1024 * 1024:
+
+                return {
+                    "error": "PDF too large. Max size is 5MB"
+                }
+
+            f.write(contents)
+
+        # =========================
+        # CREATE VECTOR STORE
+        # =========================
 
         create_vector_store(file_path)
 
@@ -24,6 +57,7 @@ async def upload_pdf(file: UploadFile = File(...)):
         }
 
     except Exception as e:
+
         return {
             "error": str(e)
         }
