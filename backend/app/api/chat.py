dbece0e770fdaeee5e0c86bfb2530466bd65f3d2
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 22:03:25 +0530

    Update chat.py

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -1,21 +1,48 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from app.agents.workflow import workflow
+
+from app.rag.vector_store import load_vector_store
+from app.services.llm_service import llm
 
 router = APIRouter()
-
 
 class ChatRequest(BaseModel):
     query: str
 
-
 @router.post("/chat")
-def chat(request: ChatRequest):
+async def chat(request: ChatRequest):
 
-    result = workflow.invoke({
-        "query": request.query
-    })
+    try:
 
-    return {
-        "response": result["report"]
-    }
+        db = load_vector_store()
+
+        docs = db.similarity_search(
+            request.query,
+            k=3
+        )
+
+        context = "\n\n".join(
+            [doc.page_content for doc in docs]
+        )
+
+        prompt = f"""
+        Answer the question using the context below.
+
+        Context:
+        {context}
+
+        Question:
+        {request.query}
+        """
+
+        response = llm.invoke(prompt)
+
+        return {
+            "response": response.content
+        }
+
+    except Exception as e:
+
+        return {
+            "error": str(e)
+        }
