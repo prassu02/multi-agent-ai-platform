57137770d2208309b03e59d8b675f6c57fcfa3bc
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 17:55:43 +0530

    Update retriever.py

--- backend/app/rag/retriever.py
+++ backend/app/rag/retriever.py
@@ -1,1 +1,7 @@
+from app.rag.vector_store import load_vector_store
 
+
+def retrieve_docs(query):
+    vectorstore = load_vector_store()
+    docs = vectorstore.similarity_search(query, k=3)
+    return docs
