f04ecacdd09d3a99a15b0fd3e7caca081d7b6e39
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 23:24:17 +0530

    Update text_splitter.py

--- backend/app/rag/text_splitter.py
+++ backend/app/rag/text_splitter.py
@@ -1,12 +1,6 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def split_documents(documents):
-
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
-    )
-
-    split_docs = text_splitter.split_documents(documents)
-
-    return split_docs
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=50
+)
