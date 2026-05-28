6391757c8caa359014e2ef4f037c760677b7618c
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 17:38:55 +0530

    Delete backend/app/services/embedding_service.py

--- backend/app/services/embedding_service.py
+++
@@ -1,7 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-vectorizer = TfidfVectorizer()
-
-def create_embeddings(chunks):
-embeddings = vectorizer.fit_transform(chunks).toarray()
-return embeddings
