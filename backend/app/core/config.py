ccf36970f57885640096b94a5d0b6ba4252c4bd9
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 17:51:08 +0530

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,9 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+UPSTASH_REDIS_REST_URL = os.getenv("UPSTASH_REDIS_REST_URL")
+UPSTASH_REDIS_REST_TOKEN = os.getenv("UPSTASH_REDIS_REST_TOKEN")
