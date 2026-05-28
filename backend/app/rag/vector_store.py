72b1a476eb119623b72be28420525b08297e5706
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 21:45:50 +0530

    Update vector_store.py

--- backend/app/rag/vector_store.py
+++ backend/app/rag/vector_store.py
@@ -1,16 +1,27 @@
 from langchain_community.vectorstores import FAISS
 from app.rag.embeddings import embedding_model
+from app.rag.document_loader import load_pdf
+from app.rag.text_splitter import split_documents
 
+DB_FAISS_PATH = "faiss_index"
 
-def create_vector_store(documents):
-    vectorstore = FAISS.from_documents(documents, embedding_model)
-    vectorstore.save_local("faiss_index")
-    return vectorstore
+def create_vector_store(pdf_path):
 
+    documents = load_pdf(pdf_path)
+
+    texts = split_documents(documents)
+
+    db = FAISS.from_documents(
+        texts,
+        embedding_model
+    )
+
+    db.save_local(DB_FAISS_PATH)
 
 def load_vector_store():
+
     return FAISS.load_local(
-        "faiss_index",
+        DB_FAISS_PATH,
         embedding_model,
         allow_dangerous_deserialization=True
     )
