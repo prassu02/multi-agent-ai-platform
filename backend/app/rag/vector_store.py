8210ef48b17552c79809f7956f3fb3b63d141a83
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 17:55:17 +0530

    Update vector_store.py

--- backend/app/rag/vector_store.py
+++ backend/app/rag/vector_store.py
@@ -1,1 +1,16 @@
+from langchain_community.vectorstores import FAISS
+from app.rag.embeddings import embedding_model
 
+
+def create_vector_store(documents):
+    vectorstore = FAISS.from_documents(documents, embedding_model)
+    vectorstore.save_local("faiss_index")
+    return vectorstore
+
+
+def load_vector_store():
+    return FAISS.load_local(
+        "faiss_index",
+        embedding_model,
+        allow_dangerous_deserialization=True
+    )
