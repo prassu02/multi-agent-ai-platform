7142d9cf0f6340c42bab91b51794f923290e7aec
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 18:02:08 +0530

    Update chat.py

--- backend/app/api/chat.py
+++ backend/app/api/chat.py
@@ -1,37 +1,21 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-
-from app.services.embedding_service import model
-from app.services.vector_store import search
-from app.services.llm_service import generate_response
+from app.agents.workflow import workflow
 
 router = APIRouter()
 
+
 class ChatRequest(BaseModel):
-    message: str
+    query: str
+
 
 @router.post("/chat")
 def chat(request: ChatRequest):
 
-    query_embedding = model.encode(request.message)
-
-    context_docs = search(query_embedding)
-
-    context = "\n".join(context_docs)
-
-    prompt = f"""
-    Use the context below to answer.
-
-    Context:
-    {context}
-
-    Question:
-    {request.message}
-    """
-
-    response = generate_response(prompt)
+    result = workflow.invoke({
+        "query": request.query
+    })
 
     return {
-        "response": response,
-        "context": context_docs
+        "response": result["report"]
     }
