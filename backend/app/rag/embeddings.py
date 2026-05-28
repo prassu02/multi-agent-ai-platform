691b10d8af389f2c029dbaf7f503419fe97225be
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-28 23:23:34 +0530

    Update embeddings.py

--- backend/app/rag/embeddings.py
+++ backend/app/rag/embeddings.py
@@ -1,6 +1,5 @@
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={"device": "cpu"}
+    model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
 )
